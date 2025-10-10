f13db38e295bf7e9df3cceb5e38356c42a75be05
joshuaaa069 <joshuaatisyt01@gmail.com>
2025-10-10 20:33:34 +1300

    manaaaa

--- app/Views/Faculty/FacultyStudent.php
+++ app/Views/Faculty/FacultyStudent.php
@@ -12,9 +12,8 @@ $this->insert('Errors/Toasts');
     </div>
 
     <!-- Subject Applications -->
-    <div class="card-glass2">
-
-        <h5>MY STUDENTS </h5>
+    <div class="card-glass2 mb-4">
+        <h5><i class="bi bi-person-lines-fill"></i> My Students</h5>
         <div>
             <hr>
             <div class="select">
@@ -38,9 +37,16 @@ $this->insert('Errors/Toasts');
                 <tbody>
                     <?php foreach ($result as $result): ?>
                         <tr>
-                            <td class="text-white"><a href="/faculty-student/studentInformation/<?= $result['student_id'] ?>" class="d-block"><?= $result['student_id'] ?> </a></td>
-                            <td class="text-white"><a href="/faculty-student/studentInformation/<?= $result['student_id'] ?>" class="d-block"><?= $result['student_lastname'] ?>, <?= $result['student_firstname'] ?> </a> </td>
-                            <td class="text-white"><a href="/faculty-student/studentInformation/<?= $result['student_id'] ?>" class="d-block"><?= $result['year_level'] ?> </a></td>
+                            <td class="text-white"><a
+                                    href="/faculty-student/studentInformation/<?= $result['student_id'] ?>"
+                                    class="d-block"><?= $result['student_id'] ?> </a></td>
+                            <td class="text-white"><a
+                                    href="/faculty-student/studentInformation/<?= $result['student_id'] ?>"
+                                    class="d-block"><?= $result['student_lastname'] ?>, <?= $result['student_firstname'] ?>
+                                </a> </td>
+                            <td class="text-white"><a
+                                    href="/faculty-student/studentInformation/<?= $result['student_id'] ?>"
+                                    class="d-block"><?= $result['year_level'] ?> </a></td>
                         </tr>
                     <?php endforeach; ?>
                 </tbody>
